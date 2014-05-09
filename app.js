0e5786ff51aea19c77107fdac33430e1c0fc40ac
Matthew Falbo <matthew.falbo@gmail.com>
2014-05-09 09:45:10 -0600

    node updates to run on heroku

--- app.js
+++ app.js
@@ -39,6 +39,9 @@ app.get('/winners', function(req, res) {
 	res.render('winner');
 });
 
-var server = app.listen(6920, function() {
+
+var port = process.env.PORT || 6920;
+
+var server = app.listen(port, function() {
 	console.log('Express server listening on port ' + server.address().port);
 });
